af0c01d96d3e20f3115626a38828f6a05418c1e6
Cristina <ana.mendoza@tecsup.edu.pe>
2023-11-17 00:38:59 -0500

    Subiendo practica

---
+++ practica03/agenda/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    avatar = models.ImageField(upload_to='contact', null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    fullname = models.CharField(max_length=100, verbose_name='Apellido')
+    email = models.EmailField(max_length=50)
+    birth = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
---
+++ practica03/agenda/views.py
@@ -0,0 +1,42 @@
+from typing import Any
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.db.models import QuerySet
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from agenda.models import Contact
+
+def base(request):
+       return render(request, 'agenda/contact_list.html')
+
+
+
+class ContactListView(generic.ListView):
+    model = Contact
+    paginate_by = 5
+
+    def get_queryset(self) -> QuerySet[Any]:
+        q = self.request.GET.get('q')
+
+        if q:
+            return Contact.objects.filter(fullname__icontains=q)
+
+        return super().get_queryset()
+
+
+class ContactCreateView(generic.CreateView):
+    model = Contact
+    fields = ('avatar', 'name','fullname', 'email', 'birth', 'phone',)
+    success_url = reverse_lazy('contact_list')
+
+
+class ContactUpdateView(generic.UpdateView):
+    model = Contact
+    fields = ('avatar', 'name', 'fullname', 'email', 'birth', 'phone',)
+    success_url = reverse_lazy('contact_list')
+
+
+class ContactDeleteView(generic.DeleteView):
+    model = Contact
+    success_url = reverse_lazy('contact_list')
